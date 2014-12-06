2a5a4ce6756ef17a8f54f1b8b0f99213c3a81c33
Igor <idvorkin@gmail.com>
2014-12-06 14:49:40 -0800

    Making it faster.

--- DumpTypedEmail.ps1
+++ DumpTypedEmail.ps1
@@ -42,29 +42,51 @@ $ol = Get-Outlook
 #paramaters
 $clipCount =10000
 $typeBlackList = "Accepted,Fwd,Canceled,Declined".Split(',')
-$folderItems = $ol.Folders.DeletedItems.Items
+$folderItems = New-Object System.Collections.ArrayList
 
 
 "Enumerating Folder, this will take minutes and slow down outlook ... "
-$Duration = Measure-Command {$folderItems = $ol.Folders.Inbox.Items | Select -First $clipCount}
+$queryFilter =  '[Received]> "11/1/2014"'
+$queryFilter
+#$duration = Measure-Command { $items = $ol.Folders.DeletedItems.Items.Restrict($queryFilter) } 
+$table = $ol.Folders.DeletedItems.GetTable($queryFilter, [Microsoft.Office.Interop.Outlook.olItemType]::olMailItem) 
+
+$table.Columns.RemoveAll()
+$m = $table.Columns.Add("Subject")
+$m = $table.Columns.Add("ReceivedTime")
+$m = $table.Columns.Add("SenderName")
+
+$countColumns = 3
+$duration = Measure-Command{ 
+    $flatTable = $table.GetArray($clipCount)
+}
+for ($row =0; $row -lt $flatTable.Count ;$row++)
+{
+    $a =@{ 
+                # TODO clean up this crappy code.
+                "Subject"=$flatTable[$row,0];
+                "ReceivedTime"=$flatTable[$row,1];
+                "SenderName"=$flatTable[$row,2] ;
+        }
+    $_ = $folderItems.Add($a)
+}
 "It took  $($duration.TotalMinutes) Minutes" 
-"Count Items [max=$clipCount]:$(($folderItems |Measure).Count)"
+"Count Items [max=$clipCount]:$($folderItems.Count)"
 
 $matcherRE =  '^(RE: )?(\w{3,})\:'
 $conciseMail = $folderItems|  
     ? {$_.Subject -match $matcherRE} | 
     Select -Property  @{Name="IsReply";Expression={$_.Subject -match 'RE:'}},
         @{Name="Type";Expression={ $matches[2] }},
-        #Subject,  #Debug only no privacy.
-        SenderName,  ReceivedTime | 
+        @{Name="SenderName";Expression={ $_.SenderName }},
+        @{Name="ReceivedTime";Expression={ $_.ReceivedTime }} |
         ? {! $typeBlackList.contains($_.Type)}
 
 
 
 "Count Concise Mails: $($($conciseMail).Count)"
-$conciseMail | Group-Object Type, IsReply | Select Count, Name
+$conciseMail | Group-Object Type, IsReply | Select Count, Name | Sort Count -Descending | Select -First 20
 
 $tmp = [System.IO.Path]::GetTempFileName() 
 $conciseMail  | Export-CSV  $tmp
-
 "Detail Messages For Analysis are in: $tmp"
