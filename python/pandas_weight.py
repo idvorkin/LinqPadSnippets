ce7ed4aefd048fec5bd7801353f388f7d5c3f247
Igor Dvorkin <idvorkin@gmail.com>
2017-09-30 18:48:14 -0700

    Code cleanup and support weight unites in lbs and kg

--- python/pandas_weight.py
+++ python/pandas_weight.py
@@ -10,22 +10,39 @@ import matplotlib
 
 %matplotlib
 
-
-
 matplotlib.style.use("ggplot")
-# Export Data from healthkit - unzip it then run:
-# https://github.com/tdda/applehealthdata
-# data goes to
 
-weight_label = "Weight in lbs"
-date_group_label = "Date" 
-EXPORTED_AND_TRANDFORMED_CSV = "c:/temp/apple_health_export/BodyMass.csv"
-df = pd.read_csv(EXPORTED_AND_TRANDFORMED_CSV)
+
+#############################################################################
+# Prepare data and import into a dataframe
+#############################################################################
+# 1) Export Data from healthkit (search internet) - unzip to a directory.
+# 2) Run code from - https://github.com/tdda/applehealthdata
+# 3) Update the script to point to your file name.
+
+exported_and_trandformed_csv_file = "c:/temp/apple_health_export/BodyMass.csv"
+df = pd.read_csv(exported_and_trandformed_csv_file)
+
+#############################################################################
+# Clean up the data.
+#############################################################################
+
+# make startDate a datetime.
 df["startDate"] = pd.to_datetime(df["startDate"])
-df[weight_label] = df["value"]
 
-# grouping is weird so switch to strings 
-df[date_group_label]=df["startDate"].apply(lambda t:f"{t.month}-{t.year-2000}")
-ax = sns.boxplot(x=date_group_label, y=weight_label, data=df)
+# and then apply stats to reasonable time period.
+date_group_column_name = "month_year" 
+df[date_group_column_name]=df["startDate"].apply(lambda t:f"{t.month}-{t.year-2000}")
+
+# Normalize weight into lbs.
+weight_lbs_column_name = "weight_in_lbs"
+df[weight_lbs_column_name] = df.apply(lambda row: row.value * 2.2 if row.unit == "kg" else row.value, axis="columns")
+
+#############################################################################
+# Make pretty pictures.
+#############################################################################
+ax = sns.boxplot(x=date_group_column_name, y=weight_lbs_column_name, data=df)
 ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
 ax.set_title("Weight over time")
+ax.set_xlabel("date")
+ax.set_ylabel("lbs")
