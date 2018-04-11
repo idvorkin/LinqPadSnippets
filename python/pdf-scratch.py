93bc3d1bef1184f55b8d4ac7f59b3492632e9eed
Igor Dvorkin <idvorkin@gmail.com>
2018-04-11 07:24:19 -0700

    Adding ftfy to pipfile, fixing pdf-scratch.

--- python/pdf-scratch.py
+++ python/pdf-scratch.py
@@ -3,16 +3,16 @@ import os
 
 from pdfrw import PdfReader, PdfWriter, PageMerge
 
-
 def makeAllUp(pages):
     result = PageMerge()
-    # NOTE: PageMerge() resizes based on content so we now have a (pageCount)*width by height page. 
-    #       Ideally we'd then resize the page, but this is fine for now since we can use print to scale in 
+    # NOTE: PageMerge() resizes based on content so we now have a (pageCount)*width by height page.
+    #       Ideally we'd then resize the page, but this is fine for now since we can use print to scale in
     #       print programs.
     for p in pages:
+        #p.scale(0.5)
         result.add(p)
         firstPageAdded = len(result) == 1
-        if firstPageAdded: continue; 
+        if firstPageAdded: continue
 
         addedPage = result[-1]
         secondLastAddedPage = result[-2]
@@ -20,7 +20,8 @@ def makeAllUp(pages):
 
     return result.render()
 
+
 inputPdf = "/temp/input.pdf"
 outputPdf = '/temp/output.pdf'
 pages = PdfReader(inputPdf).pages
-PdfWriter(output).addpage(makeAllUp(pages)).write()
+PdfWriter(outputPdf).addpage(makeAllUp(pages)).write()
