4702fa884548aa5cbc10d0e8f8c9afa690647bdf
Igor Dvorkin <idvorkin@gmail.com>
2019-06-22 10:05:09 -0700

    Make zachage tighter and executable on unix

--- python/zachage.py
+++ python/zachage.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 from datetime import date
 
 zachBirthday = date(2010, 4, 22)
 age = date.today() - zachBirthday
-weeks = age.days / 7
+ageInWeeks = age.days / 7
 
-print(f"Zach Age in weeks:{int(round(weeks))}")
+print(f"Zach Age in weeks:{round(ageInWeeks):d}")
