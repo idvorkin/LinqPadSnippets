ad54d1850ec1b9c7697752e497ba93eeafa0d52e
Igor Dvorkin <idvorkin@gmail.com>
2019-06-23 08:02:00 -0700

    Extending Zach Age to take date paramater

--- python/zachage.py
+++ python/zachage.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 from datetime import date
+from datetime import datetime
+from pyfiglet import Figlet
+import click
 
-zachBirthday = date(2010, 4, 22)
-age = date.today() - zachBirthday
-ageInWeeks = age.days / 7
 
-print(f"Zach Age in weeks:{round(ageInWeeks):d}")
+@click.command()
+@click.argument("dateoffset", default=date.today())
+def printZachAge(dateoffset: date) -> None:
+    """
+    A little tool to help compute Zach's age in week for
+    http://ig66.blogspot.com
+
+    With no paramaters uses today's date.
+    dateoffset takes format MM/DD/YY
+    """
+
+    isDatePassedIn = isinstance(dateoffset, str)
+    if isDatePassedIn:
+        dateoffset = datetime.strptime(dateoffset, "%x").date()
+
+    figlet = Figlet(font="big")
+    print(figlet.renderText("Zach Age"))
+
+    zachBirthday = date(2010, 4, 22)
+    age = dateoffset - zachBirthday
+    ageInWeeks = age.days / 7
+
+    print(f"Age in weeks:{round(ageInWeeks):d}")
+
+
+if __name__ == "__main__":
+    printZachAge()
