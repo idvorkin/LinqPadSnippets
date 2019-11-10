6fb8d013edade5faf1405e4137ada58d3b072869
Igor Dvorkin <idvorkin@gmail.com>
2019-11-10 16:45:18 +0000

    Cleanup and black

--- python/dump_grateful.py
+++ python/dump_grateful.py
@@ -3,12 +3,15 @@ import re
 import glob
 import os
 
+
 # Open all md files
 # read till hit line grateful
 # skip all lines that don't start with d.
 # copy all lines till hit  ## affirmations
+
+
 def extractGratefulFromList(l):
-    matches = re.findall("\d\.\s*(.*)", l)
+    matches = re.findall("\\d\\.\\s*(.*)", l)
     return matches
 
 
@@ -38,40 +41,47 @@ def extractGratefulFromGlob(directory):
     return results
 
 
+categories = "up early;magic;up early;diet;essential;appreciate;daily;zach;amelia;tori ".split(
+    ";"
+)
+
+
 def groupGrateful(reasons_to_be_grateful):
     grateful_by_reason = defaultdict(list)
-    categories = "up early;magic;up early;diet;essential;appreciate;daily;zach;amelia;tori ".split(';')
 
     for reason in all_reasons_to_be_grateful:
         if reason == "":
             continue
 
         normalized_reason = reason.lower()
-        key = normalized_reason
+        foundCategories = [
+            category for category in categories if category in normalized_reason
+        ]
 
-        # if it's a category then
-        for category in categories:
-            if category in normalized_reason:
-                key = category
-                break
+        isCategory = len(foundCategories) == 1
 
+        key = foundCategories[0] if isCategory else normalized_reason
         grateful_by_reason[key] += [reason]
 
-    l2 = [(k, grateful_by_reason[k]) for k in grateful_by_reason]
-    l3 = sorted(l2, key=lambda x: len(x[1]))
+    l3 = sorted(grateful_by_reason.items(), key=lambda x: len(x[1]))
     return l3
+
 
 # extractGratefulReason("a. hello world")
 # extractGratefulFromDailyFile("/home/idvorkin/gits/igor2/750words/2019-11-04.md"
 # r = dumpAll(os.path.expanduser("~/gits/igor2/750words/*md")
-#all_reasons_to_be_grateful = extractGratefulFromGlob (os.path.expanduser("~/gits/igor2/750words_new_archive/*md"))
-all_reasons_to_be_grateful = extractGratefulFromGlob (os.path.expanduser("~/gits/igor2/750words/*md"))
+# all_reasons_to_be_grateful = extractGratefulFromGlob (os.path.expanduser("~/gits/igor2/750words_new_archive/*md"))
+all_reasons_to_be_grateful = extractGratefulFromGlob(
+    os.path.expanduser("~/gits/igor2/750words/*md")
+)
 grouped = groupGrateful(all_reasons_to_be_grateful)
 
 # TODO: Consider adding a date index.
 for l in grouped:
     print(f"{l[0]}")
     isList = len(l[1]) > 1
-    if isList:
+    isCategory = any([c in l for c in categories])
+
+    if isList or isCategory:
         for m in l[1]:
             print(f"   {m}")
