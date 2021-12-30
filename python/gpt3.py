3a0ecf9fd180364120d6a872531c1542dbaf849e
Igor Dvorkin <idvorkin@gmail.com>
2021-12-30 08:28:47 -0800

    Add gpt3 command

--- python/gpt3.py
+++ python/gpt3.py
@@ -25,10 +25,30 @@ gpt3 = setup_gpt()
 app = typer.Typer()
 
 @app.command()
-def stdin(max_tokens:int=20):
+def stdin(max_tokens:int=50):
     prompt = "".join(sys.stdin.readlines())
     response_text = do_complete(prompt, max_tokens)
     print(f"[bold]{prompt}[/bold] {response_text}")
+
+@app.command()
+def tldr(max_tokens:int=50):
+    prompt_input = "".join(sys.stdin.readlines())
+    prompt=f'''My second grader asked me what this passage means:
+"""{prompt_input}
+"""
+I rephrased it for him, in plain language a second grader can understand:
+      '''
+    response = gpt3.Completion.create(engine="davinci",
+      temperature=0.5,
+      prompt=prompt,
+      max_tokens=100,
+      top_p=1,
+      frequency_penalty=0.2,
+      presence_penalty=0,
+      stop=["\"\"\""]
+)
+    response_text = response.choices[0].text
+    print(response_text)
 
 
 def do_complete(prompt, max_tokens):
