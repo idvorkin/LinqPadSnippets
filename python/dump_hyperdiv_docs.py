c95ffdd91079fc0ac028c0f75cbb0b1b5d78e378
Igor Dvorkin <idvorkin@gmail.com>
2024-09-22 16:21:06 -0700

    Update

--- python/dump_hyperdiv_docs.py
+++ python/dump_hyperdiv_docs.py
@@ -5,7 +5,6 @@ import typer
 from icecream import ic
 from rich.console import Console
 import subprocess
-import os
 import json
 from pydantic import BaseModel
 from typing import Optional
@@ -15,7 +14,7 @@ from pathlib import Path
 app = typer.Typer()
 console = Console()
 
-ast_grep_rule = """#  A rule to find the markdown to be dumped
+ast_grep_rule = """# A rule to find the markdown to be dumped
 # https://ast-grep.github.io/guide/rule-config.html#rule
 id: main
 language: python
@@ -29,7 +28,7 @@ rule:
 """
 
 def run_ast_grep():
-    # write the rule to a temporary file (cuz I can't get inline to work)
+    # write the rule to a temporary file (because I can't get inline to work)
     with tempfile.NamedTemporaryFile(mode='w', delete=True, suffix='.yaml') as temp_file:
         temp_file_path = temp_file.name
         Path(temp_file_path).write_text(ast_grep_rule)
@@ -54,9 +53,7 @@ class AstGrepHit(BaseModel):
         start: Position
         end: Position
 
-
     class MetaVariables(BaseModel):
-
         class Single(BaseModel):
             class MDType(BaseModel):
                 text: str
@@ -79,11 +76,10 @@ class AstGrepHit(BaseModel):
     note: Optional[str] = ""
     message: str
 
-
 def cleanup_output_string(s):
     # if string is wrapped with " , ' or """, remove it from the front and back
     if s.startswith('"""') and s.endswith('"""'):
-            s = s[3:-3]
+        s = s[3:-3]
     elif s.startswith("'") and s.endswith("'"):
         s = s[1:-1]
     elif s.startswith('"') and s.endswith('"'):
@@ -96,7 +92,6 @@ def cleanup_output_string(s):
     while lines and not lines[0].strip():
         lines.pop(0)
 
-
     if len(lines) >= 1 and lines[0].startswith('    '):
         # Find the minimum indentation level
         min_indent = len(lines[0]) - len(lines[0].lstrip())
@@ -104,12 +99,8 @@ def cleanup_output_string(s):
         # Remove the minimum indentation from all lines
         lines = [line[min_indent:] for line in lines]
 
-
     # Join the remaining lines back into a string
     s = '\n'.join(lines)
-
-
-
 
     return s
 
@@ -130,7 +121,8 @@ def to_doc():
         if hit.file != last_file:
             last_file = hit.file
             file_suffix = f"({last_file})"
-        print(cleanup_output_string(hit.metaVariables.single.MD.text)+file_suffix)
+        print(cleanup_output_string(hit.metaVariables.single.MD.text) + file_suffix)
 
 if __name__ == "__main__":
     app()
+
